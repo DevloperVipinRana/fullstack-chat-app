f383679815112a6ff8d63a9b10509680d956a3b0
DevloperVipinRana <vipinranavip97@gmail.com>
2025-08-06 23:32:00 +0530

    Updated environment variables and bug fixes

--- client/src/pages/auth/index.jsx
+++ client/src/pages/auth/index.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import Background from '@/assets/login2.png'
-import Victory from '@/assets/Victory.svg'
+import Victory from '@/assets/victory.svg'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
